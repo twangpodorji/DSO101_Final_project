fe891dd23ea07c3a0885a81d91325395d2849706
twangpodorji <02230311.cst@rub.edu.bt>
2025-06-18 04:42:46 +0600

    fix: nodejs in jenkinsfile naming convention3

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
   transform: {
     "^.+\\.ts$": "ts-jest",
   },
-  testMatch: ["**/tests/**/*.test.ts"],
 };
